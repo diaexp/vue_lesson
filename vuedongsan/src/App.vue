f844824de98f7031bd43239b440f3be563f1f1de
JunglePocket <bingo826284@gmail.com>
2025-09-05 22:41:05 +0900

    데이터바인딩 문법 연습

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,9 +1,31 @@
 <template>
+  <img src="./assets/logo.svg" alt="Vue logo">
+  <div>
+    원룸샵
+    <h4 class="red" :style="style">{{products[0]}} 원룸</h4>
+    <p>{{price1}} 만원</p>
+  </div>
+  <div>
+    <h4>{{products[1]}} 원룸</h4>
+    <p>{{price2}} 만원</p>
+  </div>
+  <div>
+    <h4>{{products[2]}} 원룸</h4>
+    <p>가격은아무거나</p>
+  </div>
 </template>
 
 <script>
 export default {
   name : 'App',
+  data(){
+    return {
+      price1 : 60,
+      price2 : 70,
+      style : 'color : blue',
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸']
+    }
+  },
   components : {
   }
 }
