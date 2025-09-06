bec075e049fdabcd7d542a126ab2eebb6537a01e
JunglePocket <bingo826284@gmail.com>
2025-09-06 14:36:46 +0900

    v-for 반복문을 사용해 코드 수정

--- vuedongsan/src/App.vue
+++ vuedongsan/src/App.vue
@@ -1,17 +1,15 @@
 <template>
-  <img src="./assets/logo.svg" alt="Vue logo">
-  <div>
-    원룸샵
-    <h4 class="red" :style="style">{{products[0]}} 원룸</h4>
-    <p>{{price1}} 만원</p>
-  </div>
-  <div>
-    <h4>{{products[1]}} 원룸</h4>
-    <p>{{price2}} 만원</p>
-  </div>
-  <div>
-    <h4>{{products[2]}} 원룸</h4>
-    <p>가격은아무거나</p>
+
+<div class="menu">
+  <a v-for="작명 in 메뉴들" :key="작명">{{ 작명 }}</a>
+  
+</div>
+
+  <img src="./assets/logo.svg" alt="Vue logo" width="100px" height="100px">
+
+  <div v-for="(p, i) in products" :key="i">
+    <h4 class="red" :style="style">{{ p }}</h4>
+    <p>XX 만원</p>
   </div>
 </template>
 
@@ -20,10 +18,9 @@ export default {
   name : 'App',
   data(){
     return {
-      price1 : 60,
-      price2 : 70,
       style : 'color : blue',
-      products : ['역삼동원룸', '천호동원룸', '마포구원룸']
+      products : ['역삼동원룸', '천호동원룸', '마포구원룸'],
+      메뉴들 : ['Home', 'Shop', 'About']
     }
   },
   components : {
@@ -32,4 +29,13 @@ export default {
 </script>
 
 <style>
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+}
 </style>
